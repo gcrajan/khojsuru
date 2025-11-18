4bf1b83dd17241a4e00a13e04477c52e2186b75c
Rajan GC <57903373+gcrajan@users.noreply.github.com>
2025-11-19 00:51:10 +0545

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// recruitercv/includes/config.php
+
+// --- DATABASE CREDENTIALS ---
+define('DB_HOST', 'dpg-d4e7ui6mcj7s73chg75g-a.oregon-postgres.render.com');
+define('DB_PORT', '5432'); 
+define('DB_NAME', 'khojsuru_db');
+define('DB_USER', 'khojsuru_db_user');
+define('DB_PASS', 'n7F1KVEBUfkzCguJjnXbe1FytkXqkCYt');
+
+// --- BASE URL ---
+// Change this after deployment:
+define('BASE_URL', 'https://your-app.onrender.com/');
+
+// --- ROOT PATH ---
+define('ROOT_PATH', __DIR__ . '/../');
+?>
