91ea9672310637fffbd6e154f189d1338999351d
Rajan GC <57903373+gcrajan@users.noreply.github.com>
2025-11-19 00:57:26 +0545

    config and db_connect update

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,10 +1,10 @@
-
 <?php
 // recruitercv/includes/db_connect.php
 
 require_once 'config.php';
 
-$dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+$dsn = "pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME;
+
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -13,9 +13,7 @@ $options = [
 
 try {
     $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
-} catch (\PDOException $e) {
-    // For development, showing the error is helpful.
-    // In a live production environment, you should log the error and show a generic message.
+} catch (PDOException $e) {
     die("Database Connection Error: " . $e->getMessage());
 }
 ?>
